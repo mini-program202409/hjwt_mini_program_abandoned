e0b9c62cb622b05da71c474377f6d075d001a1f2
Clear LI <lqb153105@gmail.com>
2024-10-08 22:47:49 +0800

    add user info database code
    now user info can be uploaded to wx cloud database. and also would get the database userinfo from cloud

--- miniprogram/pages/info_modification/info_modification.js
+++ miniprogram/pages/info_modification/info_modification.js
@@ -1,5 +1,6 @@
 Page({
   data: {
+    openid: '',
     name: '',
     idNumber: '',
     gender: '男',
@@ -19,6 +20,33 @@ Page({
       phone: options.phone || '',
       photo: options.photo || '',
     });
+
+    //获取openid
+    var that = this
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'login',
+      // 云函数获取结果
+      success: function(res) {
+        console.log("云函数调用成功！",res)
+        
+        //抓取数据
+        that.setData({
+          openid: res.result.userInfo.openId
+        })
+        //将数据缓存到本地
+        wx.setStorage({
+          data: that.data.openid,
+          key: 'openid',
+        })
+        //
+        let opid = wx.getStorageSync('openid')
+        console.log(opid)
+      },
+      fail: function(error) {
+        console.log("云函数调用失败！",error)
+      },
+    })
   },
 
   onInputChange(e) {
@@ -47,7 +75,15 @@ Page({
   },
 
   onSave() {
-    const { name, idNumber, gender, age, organization, phone, photo } = this.data;
+    const {
+      name,
+      idNumber,
+      gender,
+      age,
+      organization,
+      phone,
+      photo
+    } = this.data;
     if (!name || !idNumber || !age || !organization || !phone || !photo) {
       wx.showToast({
         title: '请填写完整信息',
@@ -57,7 +93,95 @@ Page({
     }
 
     // 保存信息到本地缓存
-    wx.setStorageSync('userInfo', { name, idNumber, gender, age, organization, phone, photo });
+    wx.setStorageSync('userInfo', {
+      name,
+      idNumber,
+      gender,
+      age,
+      organization,
+      phone,
+      photo
+    });
+
+
+     // 保存信息到数据库
+     const db = wx.cloud.database()
+     const _ = db.command
+     
+     // 假设 openid 是你要检测的字段
+     const openid = this.data.openid
+     console.log(openid)
+
+     // 查询数据库中是否存在相同 openid 的数据
+     db.collection('mp_register_user').where({
+       _openid: openid
+     }).get({
+       success: res => {
+         if (res.data.length > 0) {
+           // 如果存在相同 openid 的数据，则更新数据
+           db.collection('mp_register_user').doc(res.data[0]._id).update({
+             data: {
+               name: name,
+               idNumber: idNumber,
+               gender: gender,
+               age: age,
+               organization: organization,
+               phone: phone,
+               photo: photo,
+               updatedAt: db.serverDate(), // 更新时间
+             },
+             success: res => {
+               console.log('数据更新成功', res)
+               wx.showToast({
+                 title: '更新成功',
+               })
+             },
+             fail: err => {
+               console.error('数据更新失败', err)
+               wx.showToast({
+                 title: '更新失败',
+                 icon: 'none',
+               })
+             }
+           })
+         } else {
+           // 如果不存在相同 openid 的数据，则添加数据
+           db.collection('mp_register_user').add({
+             data: {
+               openid: openid, // 添加 openid 字段
+               name: name,
+               idNumber: idNumber,
+               gender: gender,
+               age: age,
+               organization: organization,
+               phone: phone,
+               photo: photo,
+               createdAt: db.serverDate(),
+             },
+             success: res => {
+               console.log('数据上传成功', res)
+               wx.showToast({
+                 title: '上传成功',
+               })
+             },
+             fail: err => {
+               console.error('数据上传失败', err)
+               wx.showToast({
+                 title: '上传失败',
+                 icon: 'none',
+               })
+             }
+           })
+         }
+       },
+       fail: err => {
+         console.error('查询数据库失败', err)
+         wx.showToast({
+           title: '查询失败',
+           icon: 'none',
+         })
+       }
+     })
 
     wx.showToast({
       title: '已保存',
--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -1,5 +1,6 @@
 Page({
   data: {
+    openid: '',
     name: '张三',
     idNumber: '123456789012345678',
     gender: '男',
@@ -21,6 +22,77 @@ Page({
     ]
   },
 
+  onLoad() {
+    //获取openid
+    var that = this
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'login',
+      // 云函数获取结果
+      success: function(res) {
+        console.log("云函数调用成功！",res)
+        
+        //抓取数据
+        that.setData({
+          openid: res.result.userInfo.openId
+        })
+        //将数据缓存到本地
+        wx.setStorage({
+          data: that.data.openid,
+          key: 'openid',
+        })
+        //
+        let opid = wx.getStorageSync('openid')
+        console.log(opid)
+      },
+      fail: function(error) {
+        console.log("云函数调用失败！",error)
+      },
+    })
+
+    //从数据库获取用户信息
+    // 获取 openid
+const openid = wx.getStorageSync('openid');
+
+if (openid) {
+  // 云开发的数据库
+  const db = wx.cloud.database();
+  const collection = db.collection('mp_register_user');
+
+  // 根据 openid 查询用户信息
+  collection.where({
+    _openid: openid
+  }).get().then(res => {
+    if (res.data.length > 0) {
+      // 获取到用户信息
+      const user = res.data[0];
+
+      // 将用户信息存储到本地数据对象 userInfo 中
+      const userInfo = {
+        name: user.name,
+        idNumber: user.idNumber,
+        gender: user.gender,
+        age: user.age,
+        organization: user.organization,
+        phone: user.phone,
+        photo: user.photo
+      };
+
+      // 将 userInfo 存储到本地缓存中（可选）
+      wx.setStorageSync('userInfo', userInfo);
+
+      // 打印 userInfo 以供调试
+      console.log('User Info:', userInfo);
+    } else {
+      console.log('User not found');
+    }
+  }).catch(err => {
+    console.error('Failed to get user info:', err);
+  });
+} else {
+  console.log('Openid not found');
+}
+  },
   onShow() {
     // 从本地存储获取最新的用户信息
     const userInfo = wx.getStorageSync('userInfo');
